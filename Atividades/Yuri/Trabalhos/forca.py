d43c5fa659868f467255adeedc6611580b3261f9
Daniel Felizardo <daniel.david772@gmail.com>
2020-03-16 02:41:58 -0300

    refactor: palavras com acentuação, são modificadas para não receberem

--- Atividades/Yuri/Trabalhos/forca.py
+++ Atividades/Yuri/Trabalhos/forca.py
@@ -55,8 +55,6 @@ def menu():
     print('='*30+'\n')
 
 
-menu()
-
 arquivo = open('arquivos/palavras_forca.txt', 'r')
 palavras = arquivo.readlines()
 
@@ -69,86 +67,96 @@ for i in range(len(palavras)):
 for j in range(0, len(lista_letras_palavras)):
     lista_letras.append(create_lista_letras_palavra(palavras[j]))
 
-continuar = True
+if amount_palavras >= 0:
+    menu()
+    continuar = True
 
-while initial_life <= total_life:
-    cont_palavra = randrange(0, amount_palavras)
+    while initial_life <= total_life:
+        cont_palavra = randrange(0, amount_palavras)
 
-    if continuar:
-        cont_letra = 0
-        palavra_incompleta = '_' * len(lista_letras[cont_palavra])
-        palavra_completa = ''
+        if continuar:
+            cont_letra = 0
+            palavra_incompleta = '_' * len(lista_letras[cont_palavra])
+            palavra_completa = ''
 
-        lista_letras_informadas = []
-        while cont_letra < len(lista_letras[cont_palavra]):
-            if initial_life > 6:
-                break
-            else:
-                if palavra_completa != '':
-                    print(
-                        f'\033[1;33mPALAVRA SECRETA -> {palavra_completa}\n')
+            lista_letras_informadas = []
+            while cont_letra < len(lista_letras[cont_palavra]):
+                if initial_life > 6:
+                    break
                 else:
-                    print(
-                        f'\033[1;33mPALAVRA SECRETA -> {palavra_incompleta}\n')
-                letra = str.lower(input('\033[1;35m> INFORME UMA LETRA -> '))
-                lista_letras_informadas.append(letra)
-
-                if check_letter_already_entered(lista_letras_informadas, letra) == False:
-                    if len(letra) == 1:
-                        if verify_letra_palavra(lista_letras[cont_palavra], letra) == False:
-                            initial_life += 1
-                        else:
-                            cont_character, indices = verify_amount_character(
-                                lista_letras[cont_palavra], letra)
-                            for indice in indices:
-                                if palavra_completa == '':
-                                    palavra_completa = palavra_incompleta[:indice] + \
-                                        letra + palavra_incompleta[indice+1:]
-                                else:
-                                    palavra_completa = palavra_completa[:indice] + \
-                                        letra + palavra_completa[indice+1:]
-                            cont_letra += cont_character
-                        if initial_life <= 6:
-                            print(
-                                f'\033[1;36m\nVIDA = {initial_life}\nCHANCES = {total_life}\n')
-                            if cont_letra == len(lista_letras[cont_palavra]):
-                                print(
-                                    f'\033[1;33mPALAVRA SECRETA -> {palavra_completa}\n')
+                    if palavra_completa != '':
+                        print(
+                            f'\033[1;33mPALAVRA SECRETA -> {palavra_completa}\n')
                     else:
                         print(
-                            f'\033[1;36m\nVIDA = {initial_life}\nCHANCES = {total_life}')
-                        print('\033[1;31m\n** Coloque apenas uma letra **\n')
-                else:
-                    print('\033[1;31m\n** LETRA JÁ INFORMADA, TENTE OUTRA **\n')
-            if cont_letra == len(lista_letras[cont_palavra]):
-                print('\033[1;32m** CONGRATULATIONS! **\n')
+                            f'\033[1;33mPALAVRA SECRETA -> {palavra_incompleta}\n')
+                    letra = str.lower(
+                        input('\033[1;35m> INFORME UMA LETRA -> '))
+                    lista_letras_informadas.append(letra)
+
+                    if check_letter_already_entered(lista_letras_informadas, letra) == False:
+                        if len(letra) == 1:
+                            if verify_letra_palavra(lista_letras[cont_palavra], letra) == False:
+                                initial_life += 1
+                            else:
+                                cont_character, indices = verify_amount_character(
+                                    lista_letras[cont_palavra], letra)
+                                for indice in indices:
+                                    if palavra_completa == '':
+                                        palavra_completa = palavra_incompleta[:indice] + \
+                                            letra + \
+                                            palavra_incompleta[indice+1:]
+                                    else:
+                                        palavra_completa = palavra_completa[:indice] + \
+                                            letra + palavra_completa[indice+1:]
+                                cont_letra += cont_character
+                            if initial_life <= 6:
+                                print(
+                                    f'\033[1;36m\nVIDA = {initial_life}\nCHANCES = {total_life}\n')
+                                if cont_letra == len(lista_letras[cont_palavra]):
+                                    print(
+                                        f'\033[1;33mPALAVRA SECRETA -> {palavra_completa}\n')
+                        else:
+                            print(
+                                f'\033[1;36m\nVIDA = {initial_life}\nCHANCES = {total_life}')
+                            print(
+                                '\033[1;31m\n** Coloque apenas uma letra **\n')
+                    else:
+                        print(
+                            '\033[1;31m\n** LETRA JÁ INFORMADA, TENTE OUTRA **\n')
+                if cont_letra == len(lista_letras[cont_palavra]):
+                    print('\033[1;32m** CONGRATULATIONS! **\n')
+                    option = str.lower(
+                        input('\033[1;31m> DESEJA CONTINUAR? S/n -> '))
+                    if option != 's':
+                        continuar = False
+                    else:
+                        lista_letras_informadas = []
+                        cont_letra = 0
+                        cont_palavra = randrange(0, amount_palavras)
+                        palavra_incompleta = '_' * \
+                            len(lista_letras[cont_palavra])
+                        palavra_completa = ''
+                        menu()
+
+            if initial_life > 6:
+                print('\033[1;36m\n** Suas chances acabaram **\n')
                 option = str.lower(
-                    input('\033[1;31m> DESEJA CONTINUAR? S/n -> '))
+                    input('\033[1;31m> Deseja recomeçar o jogo? S/n -> '))
                 if option != 's':
                     continuar = False
                 else:
                     lista_letras_informadas = []
                     cont_letra = 0
+                    initial_life = 1
                     cont_palavra = randrange(0, amount_palavras)
                     palavra_incompleta = '_'*len(lista_letras[cont_palavra])
                     palavra_completa = ''
                     menu()
+        else:
+            break
 
-        if initial_life > 6:
-            print('\033[1;36m\n** Suas chances acabaram **\n')
-            option = str.lower(
-                input('\033[1;31m> Deseja recomeçar o jogo? S/n -> '))
-            if option != 's':
-                continuar = False
-            else:
-                lista_letras_informadas = []
-                cont_letra = 0
-                initial_life = 1
-                cont_palavra = randrange(0, amount_palavras)
-                palavra_incompleta = '_'*len(lista_letras[cont_palavra])
-                palavra_completa = ''
-                menu()
-    else:
-        break
-
-print('\033[1;33m\n** OBRIGADO PELA PREFERÊNCIA, VOLTE SEMPRE **\n')
+    print('\033[1;33m\n** OBRIGADO PELA PREFERÊNCIA, VOLTE SEMPRE **\n')
+else:
+    print(
+        '\033[1;33m\n** POR FAVOR, VERIFIQUE, SE HÁ PALAVRAS NO ARQUIVO, VOLTE MAIS TARDE! **\n')
